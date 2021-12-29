84fed1eeb93f789604c6b4acf8902575e12e57df
meuar <84164343+meuar@users.noreply.github.com>
2021-12-29 05:45:54 +0300

    Update new.py

--- new.py
+++ new.py
@@ -4,6 +4,6 @@ deviceid="32F2B9820265C4165EE88F02471F2DDD673A3F0900370513ACEB515C30C7C8447E1B9B
 emaill="bzgse8z@1secmail.org"
 passwordd="987123987123"
 custompwd="987123987123"
-private="575ab7d8-d7c3-420b-a72d-6f4f740d3725"
+private="http://aminoapps.com/p/4ziv7k"
 chatlink="http://aminoapps.com/p/bh08nvh"
 nickname="mett"
